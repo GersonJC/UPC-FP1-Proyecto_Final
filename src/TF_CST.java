23d86f17061ffa04efdb970bebd465b70a22367e
Gerson <@GersonJC>
2023-09-26 22:34:34 -0500

    Metodo registar

--- src/TF_CST.java
+++ src/TF_CST.java
@@ -16,4 +16,36 @@ public class TF_CST {
 
     }
 
+    static void registrarDatos(
+            String[] dni,
+            String[] nombre,
+            String[] apellidoPaterno,
+            String[] apellidoMaterno,
+            int[] tipoHorario,
+            String[] horaSalidaReal,
+            String[] fechaRegistro,
+            int contadorRegistros,
+            String[] dniVirtual
+    ) {
+        Scanner scanner = new Scanner(System.in);
+        String dniIngresado, respuesta;
+        System.out.print("Ingrese DNI: ");
+        dniIngresado = scanner.nextLine();
+        dni[contadorRegistros] = dniIngresado;
+        System.out.print("Ingrese Nombre: ");
+        nombre[contadorRegistros] = scanner.nextLine();
+        System.out.print("Ingrese Apellido Paterno: ");
+        apellidoPaterno[contadorRegistros] = scanner.nextLine();
+        System.out.print("Ingrese Apellido Materno: ");
+        apellidoMaterno[contadorRegistros] = scanner.nextLine();
+        System.out.print("Ingrese Tipo de Horario (1, 2, o 3): ");
+        tipoHorario[contadorRegistros] = scanner.nextInt();
+        System.out.print("Ingrese Hora de Salida Real (Formato 24h / 00:00): ");
+        horaSalidaReal[contadorRegistros] = scanner.next();
+        //scanner.nextLine(); // Consumir el salto de línea
+        System.out.print("Ingrese Fecha de Registro (AAAA-MM-DD): ");
+        fechaRegistro[contadorRegistros] = scanner.next();
+    }
+
+
 }
