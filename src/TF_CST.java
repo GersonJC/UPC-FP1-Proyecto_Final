50cd78805d8921c83d16ef964147587b0739d89e
Gerson <@GersonJC>
2023-09-26 23:15:36 -0500

    Validar modalidad y mejora del main.

--- src/TF_CST.java
+++ src/TF_CST.java
@@ -4,7 +4,7 @@ public class TF_CST {
     public static void main(String[] args) {
         int opcion;
         int contadorRegistros = 0;
-
+        Scanner scanner = new Scanner(System.in);
         String[] dni = new String[100];
         String[] nombre = new String[100];
         String[] apellidoPaterno = new String[100];
@@ -14,7 +14,37 @@ public class TF_CST {
         String[] fechaRegistro = new String[100];
         String[] dniVirtual = new String[100];
 
+        do {
+            System.out.print("Seleccione una opción: ");
+            opcion = scanner.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    registrarDatos(dni, nombre, apellidoPaterno, apellidoMaterno,
+                            tipoHorario, horaSalidaReal, fechaRegistro,
+                            contadorRegistros, dniVirtual);
+                    contadorRegistros++;
+                    break;
+
+                case 2:
+                    consultarDatos(dni, nombre, apellidoPaterno, apellidoMaterno,
+                            tipoHorario, horaSalidaReal, fechaRegistro,
+                            contadorRegistros);
+                    break;
+                case 3:
+                    consolidarSobretiempo(dni, nombre, apellidoPaterno, apellidoMaterno,
+                            tipoHorario, horaSalidaReal, fechaRegistro,
+                            contadorRegistros);
+                    break;
+
+                default:
+                    System.out.println("Opción no válida. Intente de nuevo.");
+                    break;
+            }
+        } while (opcion != 5);
+
     }
+
 
     static void registrarDatos(
             String[] dni,
@@ -28,9 +58,26 @@ public class TF_CST {
             String[] dniVirtual
     ) {
         Scanner scanner = new Scanner(System.in);
+        int intentosF = 0;
         String dniIngresado, respuesta;
         System.out.print("Ingrese DNI: ");
         dniIngresado = scanner.nextLine();
+        //System.out.printf("Arreglo DNIVirtual %s \n", Arrays.toString(dniVirtual) );
+        //System.out.print("Resultado "+ validarModalidad(dniVirtual,dniIngresado));
+
+        while (!validarModalidad(dniVirtual,dniIngresado)){
+            System.out.println("DNI no cumple con la modalidad 'presencial'.");
+            System.out.print("Desea continuar (s/n): ");
+            respuesta = scanner.nextLine();
+            if (respuesta.equals("s")){
+                System.out.print("Ingrese DNI: ");
+                dniIngresado = scanner.nextLine();
+            } else if (respuesta.equals("n")) {
+                return;
+            }
+            intentosF ++;
+        };
+
         dni[contadorRegistros] = dniIngresado;
         System.out.print("Ingrese Nombre: ");
         nombre[contadorRegistros] = scanner.nextLine();
@@ -46,6 +93,16 @@ public class TF_CST {
         System.out.print("Ingrese Fecha de Registro (AAAA-MM-DD): ");
         fechaRegistro[contadorRegistros] = scanner.next();
     }
+
+    static boolean validarModalidad(String[] dniVirtual, String dni) {
+        for (int i=0; i< dniVirtual.length; i++) {
+            if(dniVirtual[i].equals(dni)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 
     static void consultarDatos(
             String[] dni,
@@ -86,6 +143,8 @@ public class TF_CST {
             System.out.println("No se encontraron registros para el DNI.");
         }
     }
+
+
 
     static void consolidarSobretiempo(
             String[] dni,
