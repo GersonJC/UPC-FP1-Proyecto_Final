552bbccfdfc073dda3394c0de2f98f3506f67cc9
Gerson <@GersonJC>
2023-09-26 20:56:47 -0500

    Arreglos y variables.

--- src/TF_CST.java
+++ src/TF_CST.java
@@ -2,6 +2,17 @@ import java.util.Scanner;
 
 public class TF_CST {
     public static void main(String[] args) {
+        int opcion;
+        int contadorRegistros = 0;
+
+        String[] dni = new String[100];
+        String[] nombre = new String[100];
+        String[] apellidoPaterno = new String[100];
+        String[] apellidoMaterno = new String[100];
+        int[] tipoHorario = new int[100];
+        String[] horaSalidaReal = new String[100];
+        String[] fechaRegistro = new String[100];
+        String[] dniVirtual = new String[100];
 
     }
 
